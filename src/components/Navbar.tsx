c77138a2859172bb2ce4ec9b0c8255b849387444
Brijesh201102 <brijesh.grabthesite2002@gmail.com>
2025-05-17 12:55:38 +0530

    done all work

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,7 +23,7 @@ const Navbar = () => {
 
   const navItems = [
     { name: 'Homes   ', href: '#home' },
-    { name: 'About', href: '#about' },
+    { name: 'Abouts', href: '#about' },
     { name: 'Skills', href: '#skills' },
     { name: 'Projects', href: '#projects' },
     { name: 'Contact', href: '#contact' }
