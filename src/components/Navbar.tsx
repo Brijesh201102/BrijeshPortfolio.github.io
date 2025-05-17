a02e0a23d19e7629f985115e9895c96199526367
Brijesh201102 <brijesh.grabthesite2002@gmail.com>
2025-05-17 12:46:12 +0530

    secondcommit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,7 +22,7 @@ const Navbar = () => {
   }, []);
 
   const navItems = [
-    { name: 'Home', href: '#home' },
+    { name: 'Homes   ', href: '#home' },
     { name: 'About', href: '#about' },
     { name: 'Skills', href: '#skills' },
     { name: 'Projects', href: '#projects' },
